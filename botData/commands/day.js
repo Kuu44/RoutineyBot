75354ac9382b3895ea39c29265bab8de977ccb05
Kushal <kushal-shrestha@hotmail.com>
2020-09-01 16:24:26 +0545

    Update day.js

--- botData/commands/day.js
+++ botData/commands/day.js
@@ -1,23 +1,22 @@
 module.exports = {
-	name: 'day',
-	description: 'Shows routine of a day!',
-	execute(message, args) {
-		const info = require('../info.json');
-		var today = new Date();
+  name: 'day',
+  description: 'Shows routine of a day!',
+  execute(message, args) {
+    const info = require('../info.json');
 
-		var msg;
-		const day=args[0];
-		msg = '> Day:' + info.days[day] + `\n> ${info.days[day]}'s Classes\n`;
-		periods = info.routine[day]._periods;
-		teachers = info.routine[day]._teachers;
+    var msg;
+    const day = args[0];
+    msg = '> Day:' + info.days[day] + `\n> ${info.days[day]}'s Classes\n`;
+    periods = info.routine[day]._periods;
+    teachers = info.routine[day]._teachers;
 
-		var i = 0;
-		while (periods[i] != "end") {
-			msg += periods[i] + '\t' + '|' + teachers[i] + '\n';
-			i++;
-		}
-		msg += '```';
+    var i = 0;
+    while (periods[i] != "end") {
+      msg += periods[i] + '\t' + '|' + teachers[i] + '\n';
+      i++;
+    }
+    msg += '```';
 
-		message.channel.send(msg);
-	},
+    message.channel.send(msg);
+  },
 };
