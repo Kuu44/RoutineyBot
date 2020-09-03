a5ce451b421f2b5b870dc28911fa0858f67f6c72
Kushal <kushal-shrestha@hotmail.com>
2020-09-03 15:08:34 +0545

    bot start

--- bot.js
+++ bot.js
@@ -19,15 +19,19 @@ for (const file of commandFiles) {
 }
 
 //for help.js
-const helpCommand=require('./functions/help.js');
-bot.commands.set(helpCommand.name,helpCommand);
+const helpCommand = require('./functions/help.js');
+bot.commands.set(helpCommand.name, helpCommand);
 
 //Bot commands
 bot.once('ready', () => { //Only triggers once after logging in
   console.log('Ready!');
 });
-//bot.login(token);
-bot.login(process.env.BOT_TOKEN);
+if (token) {
+  bot.login(token);
+}
+if (process.env.BOT_TOKEN) {
+  bot.login(process.env.BOT_TOKEN);
+}
 bot.on('message', message => {
   //logs everything typed
   console.log(message.content);
@@ -37,15 +41,14 @@ bot.on('message', message => {
   var args = message.content.slice(prefix.length).trim().split(/ +/);
   var commandName = args.shift().toLowerCase();
 
-  if(!bot.commands.has(commandName)) return message.channel.send(`Hmm I dont seem to have this command, ${message.author}!:sweat_smile:\nTry **rt! help** for more details!`);
-  const command=bot.commands.get(commandName);
-  if(command.args && !args.length){
+  if (!bot.commands.has(commandName)) return message.channel.send(`Hmm I dont seem to have this command, ${message.author}!:sweat_smile:\nTry **rt! help** for more details!`);
+  const command = bot.commands.get(commandName);
+  if (command.args && !args.length) {
     return message.channel.send(`You didnt provide any argument, ${message.author}!\nUsage: ${command.usage}`);
   }
-  try{
-    command.execute(message,args);
-  }
-  catch(error){
+  try {
+    command.execute(message, args);
+  } catch (error) {
     console.error(error);
     message.reply('There was an error trying to execute that command!');
   }
