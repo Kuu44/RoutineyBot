a0ba2b5c1c34d7e127bc02f7f1b962ff9de7c021
Kushal <kushal-shrestha@hotmail.com>
2020-09-03 19:06:03 +0545

    info update

--- info.js
+++ info.js
@@ -1,4 +1,6 @@
-{
+subjects=["Numerical Methods"];
+
+const info={
   "routine": [{
       "_emotes": [":bar_chart: ", ":bar_chart: ", ":control_knobs: ", ":control_knobs: ", "", ":construction_site: ", ":construction_site: ", ":bar_chart: ", "END"],
       "_periods": ["Numerical Methods", "Numerical Methods", "Instrumentation I", "Instrumentation I", "B", "Applied Math", "Applied Math", "Numerical Methods", "END"],
@@ -40,3 +42,4 @@
   "months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
   "Timing":[[[10,15],[11,5]],[[11,5],[11,55]],[[11,55],[12,45]],[[12,45],[13,35]],[13,35],[[13,35],[14,25]],[[14,25],[15,15]],[[15,15],[16,5]],[14,5],[14,55]]
 }
+module.exports=info;
