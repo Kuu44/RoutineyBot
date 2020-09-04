b1ade98062ea0786a28b2369735235b618980a63
Gopal-Kshetri <60227470+Gopal-Kshetri@users.noreply.github.com>
2020-09-04 19:48:47 +0545

    Update info.js

--- info.js
+++ info.js
@@ -1,12 +1,27 @@
-const subjects = ["Numerical Methods"]; //List of subjects
-const emotes = [":bar_chart: "]; //List of emotes
-const teachers = ["SG"]; //List of teachers
+const subjects = ["Numerical Methods","Instrumentation I","Applied Math","Microprocessor",
+"Data Structure and Algorithm","Discrete Structure","Electric Machine"]; //List of subjects
+const emotes = [":bar_chart:",":control_knobs:", ":construction_site:", ":robot:", ":cd:",":calendar_spiral:", ":satellite_orbital:"]; //List of emotes
+const teachers = ["SG","MB","SKM","DSB","BS","SPP","KBT","BDM","JG","JRS","LAB"]; //List of teachers
+
+//0. Numerical Methods;:bar_chart:;SG;JRS
+//1. Instrumentation I,:control_knobs:;MB;
+//2. Applied Math;:construction_site:;SG;SKM;JG
+//3. Microprocessor;:robot:;DSB;
+//4. Numerical Methods;:bar_chart:;BDM;
+//5. Data Structure and Algorithm;:cd:;BS;
+//6. Discrete Structure;:calendar_spiral:;SPP;
+//7. Electric Machine;:satellite_orbital:;KBT;
+//8. Data Structure and Algorithm;:cd:;LAB;
+//9. Microprocessor;:robot:;LAB;
+//10. Instrumentation I,:control_knobs:;LAB;
+
+
 
 const info = {
   "routine": [{
-      "_emotes": [emotes[0], ":bar_chart: ", ":control_knobs: ", ":control_knobs: ", "", ":construction_site: ", ":construction_site: ", ":bar_chart: ", "END"],
-      "_periods": [subjects[0], "Numerical Methods", "Instrumentation I", "Instrumentation I", "B", "Applied Math", "Applied Math", "Numerical Methods", "END"],
-      "_teachers": [teachers[0], "SG", "MB", "MB", "", "JG", "SG", "JRS", "END"],
+      "_emotes": [emotes[0], emotes[0], emotes[1], emotes[1], "",emotes[2], emotes[2], emotes[0], "END"],
+      "_periods": [subjects[0], subjects[0], subjects[1], subjects[1], "B", subjects[2], subjects[2], subjects[0], "END"],
+      "_teachers": [teachers[0], teachers[0], teachers[1], teachers[1], "", teachers[8], teachers[0], teachers[9], "END"],
       "_timing": [
         [
           [10, 15],
@@ -43,27 +58,9 @@ const info = {
       ]
     },
     {
-      "_emotes": [":robot:", ":robot:", ":bar_chart:", ":bar_chart: ", "", ":construction_site: ", ":cd: ", ":cd: ", ":cd:", "END"],
-      "_periods": ["Microprocessor", "Microprocessor", "Numerical Methods", "Numerical Methods", "B", "Applied Math", "Data Structure and Algorithm", "Data Structure and Algorithm", "Data Structure and Algorithm", "END"],
-      "_teachers": ["DSB", "DSB", "BDM", "BDM", "", "SKM", "LAB", "LAB", "LAB", "END"],
-      "_timing": [
-        [[10, 15],[11, 5]], [[11, 5], [11, 55]], [[11, 55], [12, 45]], [[12, 45], [13, 35]], [[13, 35], [14, 25]], [[14, 25], [15, 15]],
-        [[15, 15], [16, 5]], [[16, 5],[16, 55]]
-      ]
-    },
-    {
-      "_emotes": [":cd:", ":cd:", ":calendar_spiral:", ":calendar_spiral:", "", "END"],
-      "_periods": ["Data Structure and Algorithm", "Data Structure and Algorithm", "Discrete Structure", "Discrete Structure", "B", "END"],
-      "_teachers": ["BS", "BS", "SPP", "SPP", "", "END"],
-      "_timing": [
-        [[10, 15], [11, 5]], [[11, 5], [11, 55]], [[11, 55], [12, 45]], [[12, 45],[13, 35]],
-        [[13, 35], [14, 25]], [[14, 25], [15, 15]], [[15, 15], [16, 5]], [[16, 5], [16, 55]]
-      ]
-    },
-    {
-      "_emotes": [":control_knobs:", ":control_knobs:", ":satellite_orbital:", ":satellite_orbital:", "", ":robot:", ":robot:", ":robot:", "END"],
-      "_periods": ["Instrumentation I", "Instrumentation I", "Electric Machine", "Electric Machine", "B", "Microprocessor", "Microprocessor ", "Microprocessor ", "END"],
-      "_teachers": ["MB", "MB", "KBT", "KBT", "", "LAB", "LAB", "LAB", "END"],
+      "_emotes": [emotes[3], emotes[3], emotes[0], emotes[0], "",emotes[2], emotes[4], emotes[4], emotes[4], "END"],
+      "_periods": [subjects[3], subjects[3], subjects[0], subjects[0], "B", subjects[2], subjects[4], subjects[4], subjects[4], "END"],
+      "_teachers": [teachers[3], teachers[3], teachers[7], teachers[7], "", teachers[2], teachers[10], teachers[10], teachers[10], "END"],
       "_timing": [
         [
           [10, 15],
@@ -100,9 +97,9 @@ const info = {
       ]
     },
     {
-      "_emotes": [":satellite_orbital:", ":satellite_orbital:", ":calendar_spiral:", "", ":cd:", ":satellite_orbital:", ":satellite_orbital:", "END"],
-      "_periods": ["Electric Machine", "Electric Machine", "Discrete Structure", "B", "Data Structure and Algorithm ", "Electric Machine / :control_knobs: Instrumentation I", "Electric Machine / :control_knobs: Instrumentation I", "END"],
-      "_teachers": ["KBT", "KBT", "SPP", "", "BS", "LAB", "LAB", "END"],
+      "_emotes": [emotes[4], emotes[4], emotes[5], emotes[5], "", "END"],
+      "_periods": [subjects[4], subjects[4], subjects[5], subjects[5], "B", "END"],
+      "_teachers": [teachers[4], teachers[4], teachers[5], teachers[5], "", "END"],
       "_timing": [
         [
           [10, 15],
@@ -139,9 +136,87 @@ const info = {
       ]
     },
     {
-      "_emotes": ["", ":robot:", ":robot:", "", ":construction_site:", ":bar_chart:", ":bar_chart:", "END"],
-      "_periods": [" ", "Microprocessor", "Microprocessor", "B", "Applied Math", "Numerical Methods", "Numerical Methods", "END"],
-      "_teachers": ["", "DSB", "DSB", "", "SG", "LAB", "LAB", "END"],
+      "_emotes": [emotes[1], emotes[1], emotes[6], emotes[6], "", emotes[3], emotes[3], emotes[3], "END"],
+      "_periods": [subjects[1], subjects[1], subjects[6], subjects[6], "B", subjects[3], subjects[3], subjects[3], "END"],
+      "_teachers": [teachers[1], teachers[1], teachers[6], teachers[6], "", teachers[10], teachers[10], teachers[10], "END"],
+      "_timing": [
+        [
+          [10, 15],
+          [11, 5]
+        ],
+        [
+          [11, 5],
+          [11, 55]
+        ],
+        [
+          [11, 55],
+          [12, 45]
+        ],
+        [
+          [12, 45],
+          [13, 35]
+        ],
+        [
+          [13, 35],
+          [14, 25]
+        ],
+        [
+          [14, 25],
+          [15, 15]
+        ],
+        [
+          [15, 15],
+          [16, 5]
+        ],
+        [
+          [16, 5],
+          [16, 55]
+        ]
+      ]
+    },
+    {
+      "_emotes": [emotes[6], emotes[6], emotes[5], "", emotes[4], emotes[6], emotes[6], "END"],
+      "_periods": [subjects[6], subjects[6], subjects[5], "B", subjects[4], " Electric Machine / :control_knobs: Instrumentation I", " Electric Machine / :control_knobs: Instrumentation I", "END"],
+      "_teachers": [teachers[6], teachers[6], teachers[5], "", teachers[4], teachers[10], teachers[10], "END"],
+      "_timing": [
+        [
+          [10, 15],
+          [11, 5]
+        ],
+        [
+          [11, 5],
+          [11, 55]
+        ],
+        [
+          [11, 55],
+          [12, 45]
+        ],
+        [
+          [12, 45],
+          [13, 35]
+        ],
+        [
+          [13, 35],
+          [14, 25]
+        ],
+        [
+          [14, 25],
+          [15, 15]
+        ],
+        [
+          [15, 15],
+          [16, 5]
+        ],
+        [
+          [16, 5],
+          [16, 55]
+        ]
+      ]
+    },
+    {
+      "_emotes": ["", emotes[3], emotes[3], "",emotes[2], emotes[0], emotes[0], "END"],
+      "_periods": [" ", subjects[3], subjects[3], "B", subjects[2], subjects[0], subjects[0], "END"],
+      "_teachers": ["", teachers[3], teachers[3], "", teachers[0], teachers[10], teachers[10], "END"],
       "_timing": [
         [
           [10, 15],
