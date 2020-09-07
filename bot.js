534c3c5987452dbd3d8c1ffc0bcf73f3eb391943
Kushal <kushal-shrestha@hotmail.com>
2020-09-07 12:57:54 +0545

    v2 patch6

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ bot.commands.set(helpCommand.name, helpCommand);
 bot.once('ready', () => { //Only triggers once after logging in
   console.log('Ready!');
 });
-// bot.login(token);
+ // bot.login(token);
 bot.login(process.env.BOT_TOKEN);
 bot.on('message', message => {
   //logs everything typed
