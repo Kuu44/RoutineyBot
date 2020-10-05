fd5edcac32abb418686b47283c441eea231bf50a
Gopal-Kshetri <60227470+Gopal-Kshetri@users.noreply.github.com>
2020-10-05 21:06:59 +0545

    Update info.js

--- info.js
+++ info.js
@@ -11,6 +11,17 @@ bot-test-server 681076561142611989: #routiney-bot-test 750653645174997093
 
 Guide:
 
+BCT074:
+1. Operating System, :penguin: , AV;
+2. Database Management System, :cd: , AS;
+3. Object Oriented Analysis & Design, , AT;
+4. Artificial Intelligence, :robot: , BJ;
+5. Embedded System, , SS;
+6. Engineering Economics, , RK;
+7. Embedded System, , LAB
+8. Minor Project, , LAB;
+9. Object Oriented Analysis & Design, , LAB
+
 BCT075:
 1. Instrumentation I,:control_knobs:;MB;
 2. Applied Math;:construction_site:;SG;SKM;JG
@@ -45,9 +56,13 @@ BCT076:
 6. Thermodynamics;:thermometer: ; LAB
 7. Basic ElectrAonics Engineering;:control_knobs:; LAB
 8. Chemistry;:lab_coat:;LAB
+
 */
 
 const subjects = {
+  "BCT074": [
+    "Operating System", "Database Management System","Object Oriented Analysis & Design", "Artificial Intelligence", "Embedded System", "Engineering Economics", "Minor Project"
+  ],
   "BCT075": [
     "Numerical Methods", "Instrumentation I", "Applied Math", "Microprocessor",
     "Data Structure and Algorithm", "Discrete Structure", "Electric Machine"
@@ -61,6 +76,12 @@ const subjects = {
   ]
 }; //List of subjects
 const emotes = {
+  "BCT074": [
+     ":penguin:", ":cd:", ":robot:",":technologist:", 
+     ":control_knobs:", 
+     ":money_with_wings:",
+     ":card_box:",
+  ],
   "BCT075": [
     ":bar_chart:", ":control_knobs:", ":construction_site:", ":robot:",
     ":cd:", ":calendar_spiral:", ":satellite_orbital:"
@@ -74,11 +95,17 @@ const emotes = {
   ]
 }; //List of emotes
 const teachers = {
+  "BCT074": [
+    "AV", "AS", "BJ", "AT", "SS", "RK", "LAB"
+  ],
   "BCT075": ["SG", "MB", "SKM", "DSB", "BS", "SPP", "KBT", "BDM", "JG", "JRS", "LAB"],
   "kuus": ["SG", "MB", "SKM", "DSB", "BS", "SPP", "KBT", "BDM", "JG", "JRS", "LAB"],
   "BCT076": ["AKJ", "GG", "NA", "SP", "JKM", "NA", "JG", "AKB", "PA", "RMS", "AS", "SJ", "SST", "LAB"]
 }; //List of teachers
 const emotePNGs = {
+  "BCT074": [
+
+  ],
   "BCT075": [
     "https://hotemoji.com/images/dl/t/bar-chart-emoji-by-twitter.png",
     "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/55878/control-knobs-emoji-clipart-md.png",
@@ -108,6 +135,190 @@ const emotePNGs = {
 }; //List of emote pngs
 
 const info = {
+
+  // "550704758055370767" /*BCT074*/: {
+  //   "notificationsON": false,
+  //   "startNotification": true,
+  //   "preNotification": true,
+  //   "timeZoneFix": [5, 45],
+  //   "className": "075 BCT AB",
+  //   "classLogo": "https://i.imgur.com/OQwR8CB.png",
+  //   "classLink": "https://teams.microsoft.com/_#/school//?ctx=teamsGrid",
+  //   "preTime": 15,
+  //   "channelId": 750431836827680898, //BCT074: #routine
+    "routine": [{
+        "_emotes": [emotes['BCT074'][0], emotes['BCT074'][1], "", emotes['BCT074'][6], "END"],
+        "_emotePNGs": [emotePNGs['BCT074'][0], emotePNGs['BCT074'][1], "", emotePNGs['BCT074'][6], "END"],
+        "_periods": [subjects['BCT074'][0], subjects['BCT074'][1], "B", subjects['BCT074'][6], "END"],
+        "_teachers": [teachers['BCT074'][0], teachers['BCT074'][1], "", teachers['BCT074'][6], "END"],
+        "_timing": [
+    [
+      [10, 15],
+      [11, 55]
+    ],
+    [
+      [11, 55],
+      [13, 35]
+    ],
+    [
+      [13, 35],
+      [14, 25]
+    ],
+    [
+      [14, 25],
+      [16, 55]
+    ]
+  ]
+},
+{
+  "_emotes": [emotes['BCT074'][2], "", emotes['BCT074'][6], emotes['BCT074'][0], "END"],
+  "_emotePNGs": [emotePNGs['BCT074'][2], "", emotePNGs['BCT074'][6], emotePNGs['BCT074'][0], "END"],
+  "_periods": [subjects['BCT074'][2], "B", subjects['BCT074'][6], subjects['BCT074'][6],"END"],
+  "_teachers": [teachers['BCT074'][6], "", teachers['BCT074'][6], teachers['BCT074'][6], "END"],
+  "_timing": [
+    [
+      [10, 15],
+      [12, 45]
+    ],
+    [
+      [12, 45],
+      [13, 35]
+    ],
+    [
+      [13, 35],
+      [14, 25]
+    ],
+    [
+      [14, 25],
+      [16, 55]
+    ]
+  ]
+},
+
+{
+  "_emotes": [emotes['BCT074'][0], emotes['BCT074'][3], emotes['BCT074'][2], "", emotes['BCT074'][4], emotes['BCT074'][1], emotes['BCT074'][4],"END"],
+  "_emotePNGs": [emotePNGs['BCT074'][0], emotePNGs['BCT074'][3], emotePNGs['BCT074'][2], "", emotePNGs['BCT074'][4], emotePNGs['BCT074'][1], emotePNGs['BCT074'][4] ,"END"],
+  "_periods": [subjects['BCT074'][0], subjects['BCT074'][3], subjects['BCT074'][2], "B", subjects['BCT074'][4], subjects["BCT074"][1], subjects['BCT074'][4],"END"],
+  "_teachers": [teachers['BCT074'][0], teachers['BCT074'][3], teachers['BCT074'][2], "", teachers['BCT074'][4], teachers['BCT074'][1], teachers['BCT074'][4], "END"],
+  "_timing": [
+    [
+      [10, 15],
+      [11, 5]
+    ],
+    [
+      [11, 5],
+      [11, 55]
+    ],
+    [
+      [11, 55],
+      [12, 45]
+    ],
+    [
+      [12, 45],
+      [13, 35]
+    ],
+    [
+      [13, 35],
+      [15, 15]
+    ],
+    [
+      [15, 15],
+      [16, 5]
+    ],
+    [
+      [16, 5],
+      [16, 55]
+    ]
+  ]
+},
+
+{
+  "_emotes": [emotes['BCT074'][2], emotes['BCT074'][1], emotes['BCT074'][3], emotes['BCT074'][0], "END"],
+  "_emotePNGs": [emotePNGs['BCT074'][2], emotePNGs['BCT074'][1], emotePNGs['BCT074'][3], emotePNGs['BCT074'][0], "END"],
+  "_periods": [subjects['BCT074'][2], subjects['BCT074'][1], subjects['BCT074'][3], subjects['BCT074'][0], "END"],
+  "_teachers": [teachers['BCT074'][2], teachers['BCT074'][1], teachers['BCT074'][3], teachers['BCT074'][0], "END"],
+  "_timing": [
+    [
+      [10, 15],
+      [11, 55]
+    ],
+    [
+      [11, 55],
+      [12, 45]
+    ],
+    [
+      [12, 45],
+      [13, 35]
+    ],
+    [
+      [13, 35],
+      [14, 25]
+    ],
+    [
+      [14, 25],
+      [16, 55]
+    ]
+  ]
+},
+
+{
+  "_emotes": [emotes['BCT074'][4], " ", emotes['BCT074'][5], "", emotes['BCT074'][1], "END"],
+  "_emotePNGs": [emotePNGs['BCT074'][4]," ", emotePNGs['BCT074'][5], "", emotePNGs['BCT074'][1], "END"],
+  "_periods": [subjects['BCT074'][4], " ", subjects['BCT074'][5], "B", subjects['BCT074'][1], "END"],
+  "_teachers": [teachers['BCT074'][4], " ", teachers['BCT074'][6], "", teachers['BCT074'][1], "END"],
+  "_timing": [
+    [
+      [10, 15],
+      [11, 5]
+    ],
+    [
+      [11, 5],
+      [11, 55]
+    ],
+    [
+      [11, 55],
+      [13, 35]
+    ],
+    [
+      [13, 35],
+      [14, 25]
+    ],
+    [
+      [14, 25],
+      [16, 55]
+    ]
+  ]
+},
+
+{
+  "_emotes": [emotes['BCT074'][3], emotes['BCT074'][5], "", emotes['BCT074'][3], "END"],
+  "_emotePNGs": [emotePNGs['BCT074'][3], emotePNGs['BCT074'][5], "", emotePNGs['BCT074'][3], "END"],
+  "_periods": [subjects['BCT074'][3], subjects['BCT074'][5], "B", subjects['BCT074'][3], "END"],
+  "_teachers": [teachers['BCT074'][5], teachers['BCT074'][3], "", teachers['BCT074'][6], "END"],
+  "_timing": [
+    [
+      [10, 15],
+      [11, 55]
+    ],
+    [
+      [11, 55],
+      [13, 35]
+    ],
+    [
+      [13, 35],
+      [14, 25]
+    ],
+    [
+      [14, 25],
+      [16, 55]
+    ]
+  ]
+},
+
+
+
+
+
   "550704758055370767" /*The Collusion*/: {
     "notificationsON": false,
     "startNotification": true,
@@ -147,10 +358,10 @@ const info = {
         ]
       },
       {
-        "_emotes": [emotes['BCT075'][3], emotes['BCT075'][0], "", emotes['BCT075'][2], "", emotes['BCT075'][2], "END"],
+        "_emotes": [emotes['BCT075'][3], emotes['BCT075'][0], "", emotes['BCT075'][2], " ", emotes['BCT075'][2], "END"],
         "_emotePNGs": [emotePNGs['BCT075'][3], emotePNGs['BCT075'][0], "", emotePNGs['BCT075'][2], "", emotePNGs['BCT075'][2], "END"],
-        "_periods": [subjects['BCT075'][3], subjects['BCT075'][0], "B", subjects['BCT075'][2], "", subjects['BCT075'][2], "END"],
-        "_teachers": [teachers['BCT075'][3], teachers['BCT075'][7], "", teachers['BCT075'][2], "", teachers['BCT075'][9], "END"],
+        "_periods": [subjects['BCT075'][3], subjects['BCT075'][0], "B", subjects['BCT075'][2], " ", subjects['BCT075'][2], "END"],
+        "_teachers": [teachers['BCT075'][3], teachers['BCT075'][7], "", teachers['BCT075'][2], " ", teachers['BCT075'][9], "END"],
         "_timing": [
           [
             [10, 15],
