3a305a78c125889867d9c83c201f331fe2dcb229
Kuu44 <kushal-shrestha@hotmail.com>
2021-06-01 17:05:03 +0000

    getting data from remote database server successful

--- info.js
+++ info.js
@@ -793,45 +793,6 @@ const info = {
     ]
   },
 };
-const helpers = {
-  "days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-  "months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-  "Timing": [
-    [
-      [10, 15],
-      [11, 5]
-    ],
-    [
-      [11, 5],
-      [11, 55]
-    ],
-    [
-      [11, 55],
-      [12, 45]
-    ],
-    [
-      [12, 45],
-      [13, 35]
-    ],
-    [
-      [13, 35],
-      [14, 25]
-    ],
-    [
-      [14, 25],
-      [15, 15]
-    ],
-    [
-      [15, 15],
-      [16, 5]
-    ],
-    [
-      [16, 5],
-      [16, 55]
-    ]
-  ]
-};
 module.exports = {
-  info,
-  helpers
+  info
 };
